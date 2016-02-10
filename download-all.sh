c867b727a01b453f4fd036fdbcb38cc945d4993c
geekgit <geeekz@gmail.com>
2016-02-10 23:55:19 +0500

    Support for subfolders

--- download-all.sh
+++ download-all.sh
@@ -2,11 +2,15 @@
 git clone https://github.com/geekgit/linux_scripts
 mkdir renamed
 cd linux_scripts
-for Script in *.sh
+Scripts=$(find . -type f 2>/dev/null | grep .sh)
+echo "Scripts: $Scripts"
+for ScriptPath in $Scripts
 do
+	echo "Script path: $ScriptPath"
+	Script=$(basename $ScriptPath)
 	NewName=$(echo $Script | sed 's/.sh//g')
 	echo "$Script -> $NewName"
-	cp $Script ./../renamed/$NewName
+	cp $ScriptPath ./../renamed/$NewName
 done
 cd ./../renamed
 sudo chown root:root *
