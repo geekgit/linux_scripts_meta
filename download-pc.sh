4e38326fe389889f46cbad871b5ec4ad066a4887
geekgit <geeekz@gmail.com>
2018-10-25 12:31:20 +0000

    download-pc

---
+++ download-pc.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+sudo apt-get install -y git pv python-pip wget
+wget  --secure-protocol=TLSv1_2 --https-only https://raw.githubusercontent.com/KittyKatt/screenFetch/master/screenfetch-dev -O screenfetch-dev
+sudo cp screenfetch-dev /usr/local/bin/screenfetch-dev
+sudo chown root:root /usr/local/bin/screenfetch-dev
+sudo chmod a+rx-w /usr/local/bin/screenfetch-dev
+rm screenfetch-dev
+sudo pip install --upgrade youtube-dl
+sudo pip install --upgrade speedtest-cli
+git clone https://github.com/geekgit/linux_scripts
+mkdir renamed
+cd linux_scripts
+rm -rf .git/
+Scripts=$(find . -type f 2>/dev/null | grep .sh)
+echo "Scripts: $Scripts"
+for ScriptPath in $Scripts
+do
+	echo "Script path: $ScriptPath"
+	Script=$(basename $ScriptPath)
+	NewName=$(echo $Script | sed 's/\.sh//g')
+	# prefix	
+	NewName="geekgit-$NewName"
+	echo "$Script -> $NewName"
+	cp $ScriptPath ./../renamed/$NewName
+done
+echo "Clean /usr/local/bin..."
+cd ..
+./remove-all.sh
+cd renamed
+sudo chown root:root *
+sudo chmod a+rx-w *
+echo "Install new scripts to /usr/local/bin"
+sudo mv * /usr/local/bin
+cd ..
+rm -rf renamed/
+rm -rf linux_scripts/
+sudo geekgit-install-modem-soft
+sudo geekgit-setup-rules
