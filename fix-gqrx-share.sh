3c7f20f360af6a34e38b45051672fe81523db645
geekgit <geeekz@gmail.com>
2018-11-26 18:30:24 +0000

    gqrx fix update

--- fix-gqrx-share.sh
+++ fix-gqrx-share.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo sh -c 'cat /usr/share/gnuradio/grc/freedesktop/gnuradio-grc.xml > /usr/share/mime/packages/gnuradio.xml'
+cd /usr/share/mime/packages
+sudo wget --secure-protocol=TLSv1_2 --https-only "https://raw.githubusercontent.com/geekgit/linux_configs/master/usr/share/mime/packages/gnuradio.xml" -O "gnuradio.xml"
 cat /usr/share/mime/packages/gnuradio.xml
