49f89d59b79f4bf5d318c748de5a67a7aaab86ae
geekgit <geeekz@gmail.com>
2019-05-13 00:38:50 +0000

    nv-docker fix

--- nv-docker.sh
+++ nv-docker.sh
@@ -3,6 +3,6 @@ cd /root
 sudo wget --secure-protocol=TLSv1_2 --https-only "https://nvidia.github.io/nvidia-docker/gpgkey" -O nv-gpgkey
 sudo apt-key add nv-gpgkey
 cd /etc/apt/sources.list.d/
-sudo wget --secure-protocol=TLSv1_2 --https-only "https://nvidia.github.io/nvidia-docker/ubuntu18.04/nvidia-docker.list"
+sudo wget --secure-protocol=TLSv1_2 --https-only "https://nvidia.github.io/nvidia-docker/ubuntu18.04/nvidia-docker.list" -O nvidia-docker.list
 sudo apt-get update
 sudo apt-get install -y nvidia-docker2
