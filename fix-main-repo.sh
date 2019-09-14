a3aafc1339b18edb4a5ce785fb60d6943e355577
geekgit <geeekz@gmail.com>
2019-09-14 14:15:08 +0000

    fix-main-repo: universal way

--- fix-main-repo.sh
+++ fix-main-repo.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-./fix-main-repo-bionic.sh
+CurrPath="${PWD}"
+TmpPath="${HOME}/etc_apt_sources_$(uuidgen)"
+mkdir -p "${TmpPath}"
+cd "${TmpPath}"
+wget --secure-protocol=TLSv1_2 --https-only "https://raw.githubusercontent.com/geekgit/linux_configs/master/etc/apt/sources.template.https" -O sources.list
+cat sources.list
+Template="000template000"
+Distr=$(cat /etc/lsb-release | grep "DISTRIB_CODENAME" | awk -F= '{print $2}')
+echo "Distr: ${Distr}"
+ReplaceVal="s|${Template}|${Distr}|"
+sed -i "${ReplaceVal}" sources.list
+cat sources.list
+sudo cp sources.list /etc/apt/sources.list
+cd "${CurrPath}"
+rm -rf "${TmpPath}"
